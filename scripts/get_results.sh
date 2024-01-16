0d05760737da84fe870d8d06c8991c5e29c95f61
Siddharth <siddharth.c.vivek@gmail.com>
2024-01-16 15:00:15 +0530

    big fixes

--- scripts/get_results.sh
+++ scripts/get_results.sh
@@ -2,12 +2,13 @@
 
 remote_user="user"
 remote_ips=("10.8.1.25" "10.8.1.48" "10.8.1.44")
-remote_path="~/EkatraFL/save/$1/cifar10/$2"
-local_destination="/home/primus"
+remote_path="~/EkatraFL/save/$1/cifar10/$2/*"
+local_destination="/home/primus/Documents/DaSHLab/EkatraFL/results"
 
-# read -s -p "Enter password for $remote_user@$remote_ip: " password
 for remote_ip in ${remote_ips[*]};
     do
-        echo "user123" | scp -r -P222 "$remote_user@$remote_ip:$remote_path" "$local_destination"
+        dest_folder =  $local_destination/$1/cifar10/$2/$remote_ip
+        mkdir -p $dest_folder
+        scp -r -P222 "$remote_user@$remote_ip:$remote_path" $dest_folder
+        python evaluate_models.py $dest_folder "$2-$remote_ip"
     done
-

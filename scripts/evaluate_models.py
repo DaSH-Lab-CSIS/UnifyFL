9054e13e7615ff939532adc736b6732a347ee5ce
Sarang S <email.sarangs@gmail.com>
2024-01-19 21:11:19 +0530

    updated evalute_models.py

--- scripts/evaluate_models.py
+++ scripts/evaluate_models.py
@@ -1,51 +1,42 @@
 import os
-from torch.utils.data import DataLoader
 import csv
+from typing import List, Tuple
 import torch
 import csv
 import sys
+from torch.utils.data import DataLoader
 from models.cifar import CIFAR10Model
 
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-model=CIFAR10Model().to(DEVICE)
-local_records=[]
-global_records=[]
-
-def list_files(dir_path):
-    res = []
-    try:
-        for file_path in os.listdir(dir_path):
-            if os.path.isfile(os.path.join(dir_path, file_path)):
-                res.append(file_path)
-    except FileNotFoundError:
-        print(f"The directory {dir_path} does not exist")
-    except PermissionError:
-        print(f"Permission denied to access the directory {dir_path}")
-    except OSError as e:
-        print(f"An OS error occurred: {e}")
-    return res
-
-files = sorted(list_files(sys.argv[1]))
+model = CIFAR10Model().to(DEVICE)
 testset = model.get_testset()
 testloader = DataLoader(testset, batch_size=32)
-for i in files:
-    model.load_state_dict(torch.load(f"{sys.argv[1]}/{i}"))
+
+files = sorted(
+    [f for f in os.listdir(sys.argv[1]) if os.path.isfile(os.path.join(sys.argv[1], f))]
+)
+
+local_files = [f for f in files if f.split("-")[-1] == "local.pt"]
+global_files = [f for f in files if f.split("-")[-1] == "global.pt"]
+
+
+def evaluate_model(filename: str) -> List[float, float]:
+    model.load_state_dict(torch.load(os.path.join(sys.argv[1], filename)))
     loss, accuracy = model.test_model(testloader)
-    if i.split("-")[-1]=="local.pt":
-        local_records.append([i.split("-")[0],"-".join(i.split("-")[1:5]),accuracy,loss])
-    else:
-        global_records.append([i.split("-")[0],"-".join(i.split("-")[1:5]),accuracy,loss])
-
-file1=sys.argv[1]+"/"+sys.argv[2]+"_local.csv"
-file2=sys.argv[1]+"/"+sys.argv[2]+"_global.csv"
-with open(file1, 'a+') as csvfile:  
-    csvwriter = csv.writer(csvfile)  
-    csvwriter.writerows(local_records)
-with open(file2, 'a+') as csvfile:  
-    csvwriter = csv.writer(csvfile)  
-    csvwriter.writerows(global_records) 
+    return [accuracy, loss]
 
 
+with open(os.path.join(sys.argv[1], sys.argv[2] + "_local.csv"), "a+") as f:
+    csvwriter = csv.writer(f)
+    for file in local_files:
+        csvwriter.writerow(
+            [file.split("-")[0], "-".join(file.split("-")[1:5])] + evaluate_model(file)
+        )
 
-
+with open(os.path.join(sys.argv[1], sys.argv[2] + "_global.csv"), "a+") as f:
+    csvwriter = csv.writer(f)
+    for file in global_files:
+        csvwriter.writerow(
+            [file.split("-")[0], "-".join(file.split("-")[1:5])] + evaluate_model(file)
+        )

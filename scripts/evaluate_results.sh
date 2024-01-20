0986c3b67abf6bb132ac04ed6d236677951cae2f
Siddharth <siddharth.c.vivek@gmail.com>
2024-01-20 21:48:38 +0530

    changed bash script

--- scripts/evaluate_results.sh
+++ scripts/evaluate_results.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-remote_user="user"
 remote_ips=("10.8.1.25" "10.8.1.48" "10.8.1.44")
-remote_path="~/EkatraFL/save/$1/cifar10/$2/*"
 local_destination="/home/primus/Documents/DaSHLab/EkatraFL/results"
 
 for remote_ip in ${remote_ips[*]};
     do
         dest_folder=$local_destination/$1/cifar10/$2/$remote_ip
-        mkdir -p $dest_folder
-        scp -r -P222 "$remote_user@$remote_ip:$remote_path" $dest_folder
+        poetry run python3 scripts/evaluate_models.py $dest_folder "$2-$remote_ip"
     done

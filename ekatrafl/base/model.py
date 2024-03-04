ff82dbc396afa05bb59115b50e3cc18c73cf4423
user <f20220131@goa.bits-pilani.ac.in>
2024-03-04 08:26:58 +0530

    updated set

--- ekatrafl/base/model.py
+++ ekatrafl/base/model.py
@@ -3,13 +3,14 @@ from flwr.common.typing import NDArray, Parameters
 import torch
 from torch.utils.data.dataloader import DataLoader
 from models.cifar import CIFAR10Model
+from models.emnist import EMNISTModel
 import numpy as np
 from torch import nn
 
 import flwr as fl
 
 
-models = {"cifar10": CIFAR10Model}
+models = {"cifar10": CIFAR10Model, "emnist": EMNISTModel}
 
 
 def accuracy_scorer(model, dataloader: DataLoader):

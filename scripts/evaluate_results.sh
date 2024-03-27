04a6b3401c84ef4196a1a6e777a3b6130a408611
Druva D <ddruva445@gmail.com>
2024-03-27 15:28:50 +0530

    Improved evaluation

--- scripts/evaluate_results.sh
+++ scripts/evaluate_results.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-remote_ips=("10.8.1.25" "10.8.1.48" "10.8.1.44")
+remote_ips=("10.8.1.154" "10.8.1.48" "10.8.1.44")
 local_destination="/home/primus/Documents/DaSHLab/EkatraFL/results"
+local_destination=$4
+
+# $1 = sync/async
+# $2 = model_id
+# $3 = model_name (cifar10/mnist/emnist)
 
 for remote_ip in ${remote_ips[*]};
     do
-        dest_folder=$local_destination/$1/cifar10/$2/$remote_ip
+        dest_folder=$local_destination/$1/$3/$2/$remote_ip
         poetry run python3 scripts/evaluate_models.py $dest_folder "$2-$remote_ip"
     done
--- scripts/get_results.sh
+++ scripts/get_results.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 remote_user="user"
-remote_ips=("10.8.1.25" "10.8.1.48" "10.8.1.44")
-remote_path="~/EkatraFL/save/$1/cifar10/$2/*"
-local_destination="/home/primus/Documents/DaSHLab/EkatraFL/results"
+remote_ips=("10.8.1.154" "10.8.1.48" "10.8.1.44")
+remote_path="~/EkatraFL/save/$1/$3/$2/*"
+# local_destination="/home/primus/Documents/DaSHLab/EkatraFL/results"
+local_destination=$4
 
 for remote_ip in ${remote_ips[*]};
     do
-        dest_folder=$local_destination/$1/cifar10/$2/$remote_ip
+        dest_folder=$local_destination/$1/$3/$2/$remote_ip
         mkdir -p $dest_folder
         scp -r -P222 "$remote_user@$remote_ip:$remote_path" $dest_folder
     done
